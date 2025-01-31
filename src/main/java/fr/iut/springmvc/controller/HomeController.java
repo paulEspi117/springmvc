673334b3d6f09c69bf8f0163f3ede1a280ffdc4a
Paul Espinasse <paul.espi04@gmail.com>
2025-01-31 16:46:38 +0100

    ajout cgv

--- src/main/java/fr/iut/springmvc/controller/HomeController.java
+++ src/main/java/fr/iut/springmvc/controller/HomeController.java
@@ -2,6 +2,8 @@ package fr.iut.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
@@ -11,6 +13,12 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/cgv")
+    public String cgv() {
+        return "cgv";
+    }
     
+
+
     
 }
